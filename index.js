538c1eeb3f2b24a997761a968f154fcadeabc437
amingRo <894144279@qq.com>
2016-11-24 11:18:56 +0800

    This is my first Node Project.

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by amingRo on 2016/11/18 0018.
+ */
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route,handle);
